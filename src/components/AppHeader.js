4c4bf298389549eb9392d2111862dd96fa5b95aa
Rayan Abid <rayanabid12345@gmail.com>
2022-10-26 00:43:09 +0500

    update ui

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import {
   CContainer,
   CHeader,
@@ -11,72 +11,54 @@ import {
   CNavLink,
   CNavItem,
   CImage,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilBell, cilEnvelopeOpen, cilList, cilMenu } from '@coreui/icons'
+  CInputGroup,
+  CFormInput,
+  CRow,
+  CCol,
+  CInputGroupText,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import { cilBell, cilEnvelopeOpen, cilList, cilMenu } from "@coreui/icons";
 
-import { AppBreadcrumb } from './index'
-import { AppHeaderDropdown } from './header/index'
-import logo from 'src/assets/brand/logo.png'
+import { AppBreadcrumb } from "./index";
+import { AppHeaderDropdown } from "./header/index";
+import logo from "src/assets/brand/logo.png";
 
 const AppHeader = () => {
-  const dispatch = useDispatch()
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+  const dispatch = useDispatch();
+  const sidebarShow = useSelector((state) => state.sidebarShow);
 
   return (
     <CHeader position="sticky" className="mb-4">
       <CContainer fluid>
-        <CHeaderToggler
-          className="ps-1"
-          onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}
-        >
-          <CIcon icon={cilMenu} size="lg" />
-        </CHeaderToggler>
-        <CHeaderBrand className="mx-auto d-md-none" to="/">
-          <CImage src={logo} width={'50px'} classcName="d-inline-block align-top" alt="logo" />
+        {/* Empty div */}
+        <CHeaderNav className="d-none d-md-flex me-auto"></CHeaderNav>
+        {/* Empty div */}
 
-          {/* <CIcon icon={logo} height={48} alt="Logo" /> */}
-        </CHeaderBrand>
-        <CHeaderNav className="d-none d-md-flex me-auto">
-          <CNavItem>
-            <CNavLink to="/dashboard" component={NavLink}>
-              Dashboard
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Users</CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Settings</CNavLink>
-          </CNavItem>
-        </CHeaderNav>
         <CHeaderNav>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilBell} size="lg" />
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilList} size="lg" />
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilEnvelopeOpen} size="lg" />
-            </CNavLink>
-          </CNavItem>
+          <CRow>
+            <CCol xs={10}>
+              <CInputGroup className="">
+                <CInputGroupText
+                  style={{ background: "white", border: 0 }}
+                  id=""
+                >
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </CInputGroupText>
+                <CFormInput style={{ border: 0 }} id="" placeholder="Search" />
+              </CInputGroup>
+            </CCol>
+            <CCol xs={2}>
+              <i class="fa fa-filter headerFilterIcon" aria-hidden="true"></i>
+            </CCol>
+          </CRow>
         </CHeaderNav>
         <CHeaderNav className="ms-3">
           <AppHeaderDropdown />
         </CHeaderNav>
       </CContainer>
-      <CHeaderDivider />
-      <CContainer fluid>
-        <AppBreadcrumb />
-      </CContainer>
     </CHeader>
-  )
-}
+  );
+};
 
-export default AppHeader
+export default AppHeader;
--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,33 +1,40 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { CImage, CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
+import {
+  CImage,
+  CNavItem,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarNav,
+  CSidebarToggler,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
 
-import { AppSidebarNav } from './AppSidebarNav'
+import { AppSidebarNav } from "./AppSidebarNav";
 
-import logo from 'src/assets/brand/logo.png'
-import { sygnet } from 'src/assets/brand/sygnet'
+import logo from "src/assets/brand/logo.png";
+import { sygnet } from "src/assets/brand/sygnet";
 
-import SimpleBar from 'simplebar-react'
-import 'simplebar/dist/simplebar.min.css'
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 
 // sidebar nav config
-import navigation from '../_nav'
+import navigation from "../_nav";
 
 const AppSidebar = () => {
-  const dispatch = useDispatch()
-  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+  const dispatch = useDispatch();
+  const unfoldable = useSelector((state) => state.sidebarUnfoldable);
+  const sidebarShow = useSelector((state) => state.sidebarShow);
 
   return (
     <CSidebar
       position="fixed"
       unfoldable={unfoldable}
-      visible={sidebarShow}
-      onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
-      }}
+      // visible={sidebarShow}
+      // onVisibleChange={(visible) => {
+      //   dispatch({ type: "set", sidebarShow: visible });
+      // }}
     >
       <CSidebarBrand
         style={{
@@ -37,19 +44,27 @@ const AppSidebar = () => {
         to="/"
       >
         {/* logo */}
-        <CImage src={logo} width={'50px'} className="d-inline-block bgSider align-top" alt="logo" />
+        <CImage
+          src={logo}
+          width={unfoldable ? "50px" : "80px"}
+          className="d-inline-block bgSider align-top"
+          alt="logo"
+        />
       </CSidebarBrand>
       <CSidebarNav className="bgSider">
         <SimpleBar>
           <AppSidebarNav items={navigation} />
         </SimpleBar>
       </CSidebarNav>
+
       <CSidebarToggler
         className="d-none bgSider d-lg-flex"
-        onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+        onClick={() =>
+          dispatch({ type: "set", sidebarUnfoldable: !unfoldable })
+        }
       />
     </CSidebar>
-  )
-}
+  );
+};
 
-export default React.memo(AppSidebar)
+export default React.memo(AppSidebar);
