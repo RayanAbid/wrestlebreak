a55f403efb99f1b122815d0a25a63c4284b36d1b
Rayan Abid <68749736+RayanAbid@users.noreply.github.com>
2022-12-28 23:58:04 +0500

    Update APIUtils.js

--- src/assets/utils/APIUtils.js
+++ src/assets/utils/APIUtils.js
@@ -4,4 +4,4 @@
 // export const URL = "https://wrestling-news-api-production.up.railway.app";
 
 // Clever cloud
-export const URL = "http://app-12ed252b-e47b-4112-9f32-596fff918d76.cleverapps.io"
+export const URL = "https://app-12ed252b-e47b-4112-9f32-596fff918d76.cleverapps.io"
