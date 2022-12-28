5c4c4e371777ae217830cdfa1b3185cc3d862808
Rayan Abid <rayanabid12345@gmail.com>
2022-12-29 00:51:12 +0500

    Update APIUtils.js

--- src/assets/utils/APIUtils.js
+++ src/assets/utils/APIUtils.js
@@ -1,8 +1,8 @@
-export const URL = "http://localhost:3001";
+// export const URL = "http://localhost:3001";
 
 // railway
 // export const URL = "https://wrestling-news-api-production.up.railway.app";
 
 // clever cloud
-// export const URL =
-//   "https://app-12ed252b-e47b-4112-9f32-596fff918d76.cleverapps.io";
+export const URL =
+  "https://app-12ed252b-e47b-4112-9f32-596fff918d76.cleverapps.io";
